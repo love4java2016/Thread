ea05579b03cef865d4d27efe5638fd52fd80dcce
guopeng <guopeng600@126.com>
2016-12-27 17:13:54 +0800

    add Test

---
+++ TestThread/src/com/Test.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Test {
+    public static Integer calc(Integer para){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return para*para;
+    }
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // TODO Auto-generated method stub
+        CompletableFuture<Void> fu = CompletableFuture.supplyAsync(()->calc(50)).thenApply((i)->Integer.toString(i)).thenApply((str)->"\""+str+"\"").thenAccept(System.out::println);
+       fu.get();
+    }
+
+}
